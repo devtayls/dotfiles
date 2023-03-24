95b1f61b99c74ac053ced4664cdd7be572dacb27
devTayls <taylor.pine@pdq.com>
2023-03-23 23:33:24 -0600

    feat(nvim): remove semicolon mapping

--- neovim/.config/nvim/plugin/mappings.lua
+++ neovim/.config/nvim/plugin/mappings.lua
@@ -4,8 +4,8 @@ vim.keymap.set('n', 'N', 'Nzz')
 vim.keymap.set('n', '*', '*zz')
 vim.keymap.set('n', '#', '#zz')
 --Flip keys in normal and visual mode, so that shift isn't required for colon commands
-vim.keymap.set({'n', 'v'}, ';', ':')
-vim.keymap.set({'n', 'v'}, ':', ';')
+--vim.keymap.set({'n', 'v'}, ';', ':')
+--vim.keymap.set({'n', 'v'}, ':', ';')
 --Remap arros keys so they aren't a crutch
 vim.keymap.set('n', '<UP>', '<CMD>cope<CR>')
 vim.keymap.set('n', '<DOWN>', '<CMD>cclose<CR>')
