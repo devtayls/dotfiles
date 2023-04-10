a4bd5f324d9b9cc312e28c9e57a2bc18ff1e8df0
devTayls <taylor.pine@pdq.com>
2023-04-09 20:39:29 -0600

    neovim: add gitsigns plugin and configuration

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -94,4 +94,47 @@ return require('packer').startup(function(use)
       vim.keymap.set('n', '<leader>E', ':NvimTreeToggle<cr>')
     end
   }
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      local gitsigns = require 'gitsigns'
+
+      function next_hunk()
+        -- Move to next hunk
+        gitsigns.next_hunk()
+        -- center cursor
+        vim.cmd('normal zz')
+      end
+
+      function prev_hunk()
+        -- Move to prev hunk
+        gitsigns.prev_hunk()
+        -- center cursor
+        vim.cmd('normal zz')
+      end
+
+
+      gitsigns.setup {
+        signs = {
+          add          = {text = '│'},
+          change       = {text = '│'},
+          delete       = {text = '│'},
+          topdelete    = {text = '│'},
+          changedelete = {text = '│'},
+        },
+        on_attach = function(bufnr)
+          local map = vim.keymap.set
+          local opts = {silent = true}
+
+          map('n', ']g', next_hunk, opts)
+          map('n', '[g', prev_hunk, opts)
+          map('n', '<leader>g+', gitsigns.stage_hunk, opts)
+          map('n', '<leader>g-', gitsigns.undo_stage_hunk, opts)
+          map('n', '<leader>g=', gitsigns.reset_hunk, opts)
+          map('n', '<leader>gp', gitsigns.preview_hunk, opts)
+        end
+      }
+    end
+  }
 end)
