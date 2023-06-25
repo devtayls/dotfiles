b35c364708af143e4ff6d554c9b1f485ddcec6cb
devTayls <taylor.pine@pdq.com>
2023-06-25 13:03:42 -0600

    neovim(keymaps): add config to past from register and copy to system clipboard

--- neovim/.config/nvim/plugin/mappings.lua
+++ neovim/.config/nvim/plugin/mappings.lua
@@ -16,3 +16,9 @@ vim.keymap.set('n', '<BS>', '<CMD>b#<CR>')
 -- Select in visual mode, then copy to system keyboard 
 vim.keymap.set('v', '<leader>cc', '"+y')
 
+-- Paste from the keyboard in normal mode
+vim.keymap.set('n', '<leader>vv', '"+p')
+
+-- Put the latest yanked, persists over change and delete ops by using the 0th register
+vim.keymap.set('n', 'P', '"0p")
+
