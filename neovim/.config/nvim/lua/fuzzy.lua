fdafc09cf93eba38e3e801e26a6f239cb06cb310
devTayls <taylor.pine@pdq.com>
2023-08-01 10:08:16 -0600

    feat(which-key): add mappings for telescope settings

--- neovim/.config/nvim/lua/fuzzy.lua
+++ neovim/.config/nvim/lua/fuzzy.lua
@@ -1,30 +1,36 @@
-local telescope = require('telescope')
-local builtins = require('telescope.builtin')
+local telescope = require("telescope")
+local builtins = require("telescope.builtin")
 
 telescope.setup({
-  defaults = { 
-   prompt_prefix = '󰭎 ',
-  sorting_strategy = 'ascending',
-   layout_config = {
-      prompt_position = 'top'
-  },
+	defaults = {
+		prompt_prefix = "󰭎 ",
+		sorting_strategy = "ascending",
+		layout_config = {
+			prompt_position = "top",
+		},
 	},
 	pickers = {
-    find_files = {
-		  find_command = {
-				'rg', '--files', '--hidden', '-g', '!.git', '--follow'
-			}  
-		}, 
-    buffers = {
-      show_all_buffers = true
-    },
-	}
+		find_files = {
+			find_command = {
+				"rg",
+				"--files",
+				"--hidden",
+				"-g",
+				"!.git",
+				"--follow",
+			},
+		},
+		buffers = {
+			show_all_buffers = true,
+		},
+	},
 })
 
--- Define some mappings
--- TODO: describe leader key
--- TODO: describe mappings
-vim.keymap.set('n', '<leader><leader>', builtins.find_files)
-vim.keymap.set('n', '<leader>/', builtins.live_grep)
-vim.keymap.set('n', '<leader>gs', builtins.git_status)
-vim.keymap.set('n', '<leader><Backspace>', builtins.buffers)
+local wk = require("which-key")
+
+wk.register({
+	["<leader><leader>"] = { builtins.find_files, "Files" },
+	["<leader>/"] = { builtins.live_grep, "GREP" },
+	["<leader>gs"] = { builtins.git_status, "git status" },
+	["<leader><Backspace>"] = { builtins.buffers, "Buffers" },
+})
