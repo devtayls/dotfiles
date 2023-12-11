d10bd427ec7ebbc446e92f5957092dcd31d95f8c
devTayls <taylor.pine@pdq.com>
2023-12-11 14:13:09 -0700

    neovim(lsp): fix lanugage server commands and whichkey config

--- neovim/.config/nvim/lua/language_servers.lua
+++ neovim/.config/nvim/lua/language_servers.lua
@@ -1,6 +1,9 @@
 local lsp_config = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local null_ls = require("null-ls")
+local configs = require("lspconfig.configs")
+
+local formatting_augrop = vim.api.nvim_create_augroup("LSPFORMATTING", {})
 
 local attach = function(client, buffer_number)
 	local options = {
@@ -18,6 +21,7 @@ local attach = function(client, buffer_number)
 			["O"] = { vim.lsp.buf.document_symbol, "quicklist sym" },
 			-- Open Telscope of Symbols
 			["o"] = { ":Telescope lsp_document_symbols<CR>", "telescop sym" },
+			-- Format buffer
 			["f"] = { vim.lsp.buf.format, "format" },
 			["r"] = { vim.lsp.buf.references, "references" },
 			["s"] = { vim.lsp.buf.signature_help, "sig help" },
@@ -26,14 +30,6 @@ local attach = function(client, buffer_number)
 
 	-- Enter to go to definition
 	vim.keymap.set("n", "<CR>", vim.lsp.buf.definition, options)
-
-	-- show hover window of definition
-	-- vim.keymap.set("n", "<LEADER>lh", vim.lsp.buf.hover, options)
-	-- vim.keymap.set("n", "<LEADER>lO", vim.lsp.buf.document_symbol, options)
-	-- -- show Telescop window listing lsp object
-	-- vim.keymap.set("n", "<LEADER>lo", ":Telescope lsp_document_symbols<CR>", options)
-
-	local formatting_augrop = vim.api.nvim_create_augroup("LSPFORMATTING", {})
 
 	if client.supports_method("textDocument/formatting") then
 		print(client.name)
@@ -48,16 +44,30 @@ local attach = function(client, buffer_number)
 	end
 end
 
-lsp_config.elixirls.setup({
-	cmd = { "elixir-ls" },
-	on_attach = attach,
-	capabilities = capabilities,
-	settings = {
-		elixirLS = {
-			dialyzerEnabled = true,
+if not configs.lexical then
+	configs.lexical = {
+		default_config = {
+			filetypes = { "elixir", "eelixir", "heex" },
+			cmd = { vim.loop.os_homedir() .. "/code/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+			root_dir = function(fname)
+				return lsp_config.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+			end,
 		},
-	},
-})
+	}
+end
+
+lsp_config.lexical.setup({ on_attach = attach })
+
+-- lsp_config.elixirls.setup({
+-- 	cmd = { "elixir-ls" },
+-- 	on_attach = attach,
+-- 	capabilities = capabilities,
+-- 	settings = {
+-- 		elixirLS = {
+-- 			dialyzerEnabled = true,
+-- 		},
+-- 	},
+-- })
 
 lsp_config.vale_ls.setup({
 	on_attach = attach,
