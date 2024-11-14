3ad44f937b35c4bef87155a343669070d99843b1
devTayls <taylor.pine@pdq.com>
2024-11-14 10:04:21 -0700

    feat(file-paths): add tmux-file-paths helper and telescope integration

--- neovim/.config/nvim/lua/plugins/fuzzy.lua
+++ neovim/.config/nvim/lua/plugins/fuzzy.lua
@@ -10,6 +10,9 @@ return {
 		config = function()
 			local telescope = require("telescope")
 			local builtins = require("telescope.builtin")
+			local pickers = require("telescope.pickers")
+			local sorters = require("telescope.sorters")
+			local finders = require("telescope.finders")
 
 			telescope.setup({
 				defaults = {
@@ -50,15 +53,39 @@ return {
 				},
 			})
 
-			local wk = require("which-key")
+			-- tmux-file-paths helper
+			local function get_file_paths()
+				local picker = pickers.new({
+					finder = finders.new_oneshot_job({ "tmux-file-paths" }, {
+						entry_maker = function(entry)
+							local _, _, filename, lnum = string.find(entry, "(.+):(%d+)")
 
-			wk.register({
-				["<leader><leader>"] = { builtins.find_files, "files" },
-				["<leader>/"] = { builtins.live_grep, "fzf" },
-				["<leader>gs"] = { builtins.git_status, "changed files" },
-				["<leader><Backspace>"] = { builtins.buffers, "buffers" },
-				["<leader>f"] = { name = "+find" },
-				["<leader>fi"] = { "<CMD>Telescope symbols<CR>", "🤓" },
+							return {
+								value = entry,
+								ordinal = entry,
+								display = entry,
+								filename = filename,
+								lnum = tonumber(lnum),
+								col = 0,
+							}
+						end,
+					}),
+					sorter = sorters.get_generic_fuzzy_sorter(),
+					previewer = require("telescope.previewers").vim_buffer_vimgrep.new({}),
+				})
+
+				picker:find()
+			end
+
+			local wk = require("which-key")
+			wk.add({
+				{ "<leader>f", group = "+find" },
+				{ "<leader>fi", "<CMD>Telescope symbols<CR>", desc = "🤓" },
+				{ "<leader><leader>", builtins.find_files, desc = "telescope files" },
+				{ "<leader>/", builtins.live_grep, desc = "telescope fzf" },
+				{ "<leader>gs", builtins.git_status, desc = "telescope changed files" },
+				{ "<leader>fp", get_file_paths, desc = "telescope file paths" },
+				{ "<leader><Backspace>", builtins.buffers, desc = "telescope buffers" },
 			})
 		end,
 	},
