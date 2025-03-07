39e2cfa103422ba30991e81f389f30eafb029845
devTayls <taylor.pine@pdq.com>
2025-03-07 13:06:31 -0700

    feat(neovim): add lazy loading to which-key plugin

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -117,6 +117,17 @@ return {
 		-- 	-- or leave it empty to use the default settings
 		-- 	-- refer to the configuration section below
 		-- })
+		lazy = false,
+		-- opts = {
+		-- }
+		-- config = function()
+		-- 	vim.o.timeout = true
+		-- 	vim.o.timeoutlen = 300
+		-- 	require("which-key").setup({
+		-- 		-- your configuration comes here
+		-- 		-- or leave it empty to use the default settings
+		-- 		-- refer to the configuration section below
+		-- 	})
 		-- end,
 	},
 }
