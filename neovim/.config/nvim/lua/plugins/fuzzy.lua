bbae90e9ed461fe5866dc593e767d517a2fc9fb9
devTayls <taylor.pine@pdq.com>
2026-04-19 21:56:34 -0600

    refactor(nvim): use nvim-tree/nvim-web-devicons in telescope deps

--- neovim/.config/nvim/lua/plugins/fuzzy.lua
+++ neovim/.config/nvim/lua/plugins/fuzzy.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"kyazdani42/nvim-web-devicons",
+			"nvim-tree/nvim-web-devicons",
 			"nvim-telescope/telescope-symbols.nvim",
 			"folke/which-key.nvim",
 			"mrloop/telescope-git-branch.nvim",
